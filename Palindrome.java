5a65f207bc01483afcf1166b6a604adb1c8dcd13
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-01-02 19:44:34 -0500

    palindrome java

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+// Palindrome program wiithour converting to string.
+
+// Take care of -ve nos and integer overflow
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+    if(x<0 || (x!=0 && x%10==0))
+        return false;
+        while(x>rev)
+        {
+            rev=rev*10+x%10;
+            x=x/10;
+        }
+        return (x==rev || x==rev/10);
+        
+    }
+}
