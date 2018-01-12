30e56b28d0860bc968072d285856bf9c9b409c5b
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-01-12 13:28:42 -0500

    Record breaking game.
    Record breaking java.

---
+++ breaking_the_records.java
@@ -0,0 +1,74 @@
+/*
+Maria plays  games of college basketball in a season. Because she wants to go pro, she tracks her points scored per game sequentially in an array defined as . After each game , she checks to see if score  breaks her record for most or least points scored so far during that season.
+
+Given Maria's array of  for a season of  games, find and print the number of times she breaks her record for most and least points scored during the season.
+
+Note: Assume her records for most and least points at the start of the season are the number of points scored during the first game of the season.
+
+Input Format
+
+The first line contains an integer denoting  (the number of games). 
+The second line contains  space-separated integers describing the respective values of .
+
+Output Format
+
+Print two space-seperated integers describing the respective numbers of times her best (highest) score increased and her worst (lowest) score decreased.
+
+Sample Input 0
+
+9
+10 5 20 20 4 5 2 25 1
+Sample Output 0
+
+2 
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] breakingRecords(int[] score) {
+        // Complete this function
+        int maxBreak = 0, minBreak=0;
+        int max = score[0], min = score[0];
+        for(int i=1;i<score.length;i++)
+        {
+            if(score[i]<min)
+            {
+                min = score[i];
+                minBreak++;
+            }
+            
+            if(score[i]>max)
+            {
+                max = score[i];
+                maxBreak++;
+            }
+            
+        }
+        return new int[] {maxBreak, minBreak};
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] score = new int[n];
+        for(int score_i = 0; score_i < n; score_i++){
+            score[score_i] = in.nextInt();
+        }
+        int[] result = breakingRecords(score);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+}
+
+
