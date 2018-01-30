8d0560217c77e1659951ca34debf3deb68071a5a
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-01-29 22:37:07 -0500

    maxperimeter.java
    Java code to find the maximum perimemter in alist of triangles.

---
+++ max_perimeter.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        scanner.close();
+        Arrays.sort(a);
+        int x, y, z;
+        for(x=n-3, y=n-2, z=n-1; a[x]+a[y]<=a[z]; x--, y--, z--){
+            if(x==0) {
+                System.out.print(-1);
+                return;
+            }
+        }
+        System.out.printf("%d %d %d", a[x], a[y], a[z]);
+    }
+}
