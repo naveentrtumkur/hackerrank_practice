8fd4c60efb5143fa9ee6739c713fd1c9bda21ebe
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-03-05 11:40:35 -0500

    Finding the minimum depth of a binary tree.
    a program to find the min depth of BT

---
+++ min_Depth_BT.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+   
+    public int minDepth(TreeNode root) {
+        
+        // First if root is null, return 0
+        if(root == null)
+            return 0;
+        
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        
+        return (leftDepth == 0 || rightDepth == 0 )? leftDepth + rightDepth + 1 : 1 + Math.min(leftDepth, rightDepth);
+}
+}
